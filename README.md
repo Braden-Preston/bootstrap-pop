b698a9da4dcd6edbaf9ff062909cdbe38b278d76
Braden-Preston <bpfilmsinc@gmail.com>
2021-11-14 20:40:36 -0600

    update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Bootstrap Pop!
 
+A quick post-install script for Pop! OS to get it ready for most coding and visual effects tasks
+
 ![](docs/preview.png)
 
-# Getting Started
+## Getting Started
 
 Clone the repository and run the install command
 
@@ -10,3 +12,26 @@ Clone the repository and run the install command
 git clone https://github.com/Braden-Preston/bootstrap-pop.git ~/.bootstrap
 ~/.bootstrap/install.sh
 ```
+
+## Removal
+
+Will only reset themes and preferences back to default. Will not remove applications or their preferences.
+
+```bash
+~/.bootstrap/remove.sh
+```
+
+## What's Inside
+
+- Adjustments to Cosmic Desktop
+- Installation of Gnome Tweaks
+- Papirus Icons w/ BlueGrey folders
+- Nordic Polar/Darker GTK Themes
+- MacOS-like task runner with ArcMenu
+
+Utilities
+
+- htop, openrgb, gparted
+
+Apps
+- VSCode, Blender, Spotify, Discord, Telegram, Gimp, Inkscape
--- install.sh
+++ install.sh
@@ -14,11 +14,28 @@ dir=`pwd $0`
 
 echo -e '\e[1;34mInstall started\e[0m'
 
-echo '...drivers'
-echo '...extensions'
-echo '...applications'
-echo '...theme & icons'
-echo '...preferences'
+mkdir -p ~/Documents/GitHub
+mkdir -p ~/.themes
+mkdir -p downloads
+
+
+# Install Theme and Icons
+
+mkdir -p ~/.themes/Nordic
+curl -sL -o downloads/Nordic.tar.xz https://github.com/EliverLara/Nordic/releases/latest/download/Nordic.tar.xz
+tar -xf downloads/Nordic.tar.xz --directory ~/.themes/Nordic
+# gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+# gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+
+# rsync -rq downloads/Nordic ~/.themes
+
+# git clone -q https://github.com/EliverLara/Nordic.git ~/Documents/GitHub/Nordic
+
+# echo '... drivers'
+# echo '... extensions'
+# echo '... applications'
+# echo '... theme & icons'
+# echo '... preferences'
 
 echo -e '\e[1;32mInstall complete\e[0m'
 
